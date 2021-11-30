518abef8fdd1943a73844cf2b6ae73468c4d804f
jb <33707246+JosephBur@users.noreply.github.com>
2021-11-30 12:48:51 -0700

    Update pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 // RemoteXY select connection mode and include library
-#define REMOTEXY_MODE__SOFTSERIAL
+#define REMOTEXY_MODE__HARDSERIAL
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -8,39 +8,35 @@
 #include <SPI.h>
 
 // RemoteXY connection settings
-#define REMOTEXY_SERIAL_RX 2
-#define REMOTEXY_SERIAL_TX 3
+#define REMOTEXY_SERIAL Serial
 #define REMOTEXY_SERIAL_SPEED 9600
 
 #include <SoftwareSerial.h>
 #include <RemoteXY.h>
 
 // RemoteXY PIN settings
-#define PIN_FORWARD_LEFT 7
+#define PIN_FORWARD_LEFT 5
 #define PIN_BACKWARD_LEFT 9
-#define PIN_FORWARD_RIGHT 8
+#define PIN_FORWARD_RIGHT 6
 #define PIN_BACKWARD_RIGHT 10
 
 // RemoteXY configurate
 #pragma pack(push, 1)
 uint8_t RemoteXY_CONF[] =
-  { 255,7,0,14,0,90,0,13,178,0,
-  130,2,41,34,19,23,138,4,0,47,
-  36,7,18,207,26,5,32,35,3,30,
-  30,138,26,31,65,114,90,1,9,9,
-  67,1,45,53,11,4,207,26,11,1,
-  0,5,12,16,16,137,31,226,172,134,
-  0,1,0,5,37,16,16,137,31,226,
-  172,135,0,1,0,79,12,16,16,137,
-  31,226,172,134,0,1,0,79,37,16,
-  16,137,31,226,172,135,0 };
+  { 255,5,0,14,0,81,0,13,178,0,
+  130,2,41,25,19,32,138,4,0,47,
+  26,7,28,38,26,65,114,90,1,9,
+  9,67,1,45,53,11,4,38,26,11,
+  1,0,5,12,16,16,137,31,226,172,
+  134,0,1,0,5,37,16,16,137,31,
+  226,172,135,0,1,0,79,12,16,16,
+  137,31,226,172,134,0,1,0,79,37,
+  16,16,137,31,226,172,135,0 };
 
 // this structure defines all the variables and events of your control interface
 struct {
     // input variables
   int8_t max_speed; // =0..100 slider position
-  int8_t joystick_1_x; // =-100..100 x-coordinate joystick position
-  int8_t joystick_1_y; // =-100..100 y-coordinate joystick position
   uint8_t forward_left; // =1 if button pressed, else =0
   uint8_t backward_left; // =1 if button pressed, else =0
   uint8_t forward_right; // =1 if button pressed, else =0
@@ -68,17 +64,17 @@ Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
 void setup() {
   RemoteXY_Init();
 
-  pinMode (PIN_FORWARD_LEFT, OUTPUT);
-  pinMode (PIN_BACKWARD_LEFT, OUTPUT);
-  pinMode (PIN_FORWARD_RIGHT, OUTPUT);
-  pinMode (PIN_BACKWARD_RIGHT, OUTPUT);
+  pinMode(PIN_FORWARD_LEFT, OUTPUT);
+  pinMode(PIN_BACKWARD_LEFT, OUTPUT);
+  pinMode(PIN_FORWARD_RIGHT, OUTPUT);
+  pinMode(PIN_BACKWARD_RIGHT, OUTPUT);
 
   // pinMode(7, INPUT);
   // pinMode(9, INPUT);
   // pinMode(8, INPUT);
   // pinMode(10, INPUT);
 
-  RemoteXY.max_speed = 100;
+  RemoteXY.max_speed = 50;
 
   AFMS.begin();
 }
@@ -99,13 +95,12 @@ void loop() {
   uint8_t speed = ceil(slider_pos * 2.55);
   sprintf(RemoteXY.max_speed_text, "%u", slider_pos);
 
-  // pin 7 and 9 are the left side motors
-  if (digitalRead(7) == HIGH) {
+  // pin 5 and 9 are the left side motors
+  if (digitalRead(5) == HIGH) {
     myMotor3->setSpeed(speed);
     myMotor3->run(FORWARD);
     myMotor4->setSpeed(speed);
     myMotor4->run(FORWARD);
-
   } else if (digitalRead(9) == HIGH) {
     myMotor3->setSpeed(speed);
     myMotor3->run(BACKWARD);
@@ -116,15 +111,13 @@ void loop() {
     myMotor3->run(RELEASE);
     myMotor4->setSpeed(0);
     myMotor4->run(RELEASE);
-
   }
-  // pin 8 and 10 are the right side motors
-  if (digitalRead(8) == HIGH) {
+  // pin 6 and 10 are the right side motors
+  if (digitalRead(6) == HIGH) {
     myMotor2->setSpeed(speed);
     myMotor2->run(FORWARD);
     myMotor1->setSpeed(speed);
     myMotor1->run(FORWARD);
-
   } else if (digitalRead(10) == HIGH) {
     myMotor2->setSpeed(speed);
     myMotor2->run(BACKWARD);
@@ -135,6 +128,5 @@ void loop() {
     myMotor2->run(RELEASE);
     myMotor1->setSpeed(0);
     myMotor1->run(RELEASE);
-
   }
 }
